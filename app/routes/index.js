47149bde8920c143b201f38ef43398bfacdc05d6
glass eagle <mario.torres2@yahoo.com>
2016-06-21 07:37:11 +0000

    added /new/* route

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,13 +2,19 @@
 
 var path = process.cwd();
 var ClickHandler = require(path + '/app/controllers/clickHandler.server.js');
+var newURLHandler = require(path + '/app/controllers/newURLHandler.server.js');
 
 module.exports = function (app) {
 
 	var clickHandler = new ClickHandler();
 
+	app.route('/new/*')
+		.get(newURLHandler);
+
 	app.route('/')
 		.get(function (req, res) {
 			res.sendFile(path + '/public/index.html');
 		});
+		
+	
 };
