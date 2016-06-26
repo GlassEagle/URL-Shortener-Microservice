aec01fc4f17b5c3f7e5d6c219a851558f0caa977
glass eagle <mario.torres2@yahoo.com>
2016-06-26 11:59:50 +0000

    added djb alphanumberic hash to short_url

--- app/controllers/newURLHandler.server.js
+++ app/controllers/newURLHandler.server.js
@@ -1,10 +1,30 @@
 'use strict';
 
 var ShortURLs = require("../models/shortURLs.js");
+var bling = require('bling-hashes');
 
 function newURLHandler (req, res) {
 	
-	var shortURL = new  ShortURLs({original_url: req.params[0]});
+	var djbHash = bling.djb(req.params[0]); //djb hash
+	var arr = [];
+	var char;
+	var q = djbHash;
+	var r;
+	
+	//change the number djbHash to alphanumeric numbering (62 characers)
+	while(q > 0){
+		r = q%62; //remainder
+		q = Math.floor(q/62); //quotient
+		
+		char = (r < 10) ? String.fromCharCode(48 + r) //char is 0-9
+				: (r < 36) ? String.fromCharCode(55 + r) //char is A-Z
+				: String.fromCharCode(61 + r); //char is a-z
+				
+		arr.unshift(char); //add char to the front
+	}
+	var url_id = arr.join(""); //join array of characters into a string
+	
+	var shortURL = new  ShortURLs({original_url: req.params[0], short_url: url_id});
 	
 	shortURL.save(function(err, product){
 		if(err)
