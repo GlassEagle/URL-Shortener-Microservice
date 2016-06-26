ef152e696a418dbb2846a522a815c97df97574db
glass eagle <mario.torres2@yahoo.com>
2016-06-26 09:46:32 +0000

    validate original_url

--- app/models/shortURLs.js
+++ app/models/shortURLs.js
@@ -2,10 +2,15 @@
 
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
+var url = require('url');
 
 var shortURL = new Schema({
-    original_url: String,
+    original_url: {type: String, validate: [validateOriginalURL, "Wrong url format, make sure you have a valid protocol and real site."]},
     short_url: Number
 });
+
+function validateOriginalURL(val){
+    return url.parse(val).hostname;
+}
 
 module.exports = mongoose.model('ShortURL', shortURL);
