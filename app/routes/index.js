5afe4040a2f6ce4342baef883880b454ab4974c6
glass eagle <mario.torres2@yahoo.com>
2016-06-27 00:51:57 +0000

    removed clickHandler

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,12 +1,9 @@
 'use strict';
 
 var path = process.cwd();
-var ClickHandler = require(path + '/app/controllers/clickHandler.server.js');
 var newURLHandler = require(path + '/app/controllers/newURLHandler.server.js');
 
 module.exports = function (app) {
-
-	var clickHandler = new ClickHandler();
 
 	app.route('/new/*')
 		.get(newURLHandler);
